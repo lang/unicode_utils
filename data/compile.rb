65bb4458c4dce1a3d81f838e84bcfbd608e7bd34
Stefan Lang <langstefan@gmx.at>
2008-10-25 20:26:31 +0200

    add data/compile.rb and compiled data files

---
+++ data/compile.rb
@@ -0,0 +1,82 @@
+# encoding: utf-8
+
+module UnicodeUtils
+
+  Codepoint = Struct.new(:codepoint,
+                         :name,
+                         :simple_lowercase_mapping,
+                         :simple_uppercase_mapping)
+
+  class Compiler
+
+    def initialize
+      @basedir =
+        File.absolute_path(File.join(File.dirname(__FILE__), ".."))
+      @datadir = File.join(@basedir, "data")
+      @libdir = File.join(@basedir, "lib", "unicode_utils")
+    end
+
+    def each_codepoint
+      data_fn = File.join(@datadir, "UnicodeData.txt")
+      File.open(data_fn, "r:US-ASCII") do |io|
+        io.each_line { |line|
+          yield parse_line(line)
+        }
+      end
+      nil
+    end
+
+    def parse_line(line)
+      Codepoint.new.tap { |cp|
+        fields = line.split(";")
+        cp.codepoint = fields[0].to_i(16)
+        cp.name = fields[1]
+        uc_mapping = fields[12]
+        unless uc_mapping.empty?
+          cp.simple_uppercase_mapping = uc_mapping.to_i(16)
+        end
+        lc_mapping = fields[13]
+        unless lc_mapping.empty?
+          cp.simple_lowercase_mapping = lc_mapping.to_i(16)
+        end
+      }
+    end
+
+    def run
+      uc_file =
+        File.open(File.join(@libdir, "simple_uc_map"), "w:US-ASCII")
+      lc_file =
+        File.open(File.join(@libdir, "simple_lc_map"), "w:US-ASCII")
+      name_file =
+        File.open(File.join(@libdir, "names"), "w:US-ASCII")
+      begin
+        each_codepoint { |cp|
+          if cp.simple_uppercase_mapping
+            uc_file.write(format_codepoint(cp.codepoint))
+            uc_file.write(format_codepoint(cp.simple_uppercase_mapping))
+          end
+          if cp.simple_lowercase_mapping
+            lc_file.write(format_codepoint(cp.codepoint))
+            lc_file.write(format_codepoint(cp.simple_lowercase_mapping))
+          end
+          name_file.write(format_codepoint(cp.codepoint))
+          name_file.puts(cp.name)
+        }
+      ensure
+        uc_file.close
+        lc_file.close
+        name_file.close
+      end
+    end
+
+    def format_codepoint(cp)
+      sprintf("%06x", cp)
+    end
+
+  end
+
+end
+
+if $0 == __FILE__
+  UnicodeUtils::Compiler.new.run
+end

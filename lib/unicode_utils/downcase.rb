2005d34489b8d0e4bb246ddd378a5a9f9e5db217
Stefan Lang <langstefan@gmx.at>
2008-10-26 22:51:19 +0100

    document upcase and downcase

--- lib/unicode_utils/downcase.rb
+++ lib/unicode_utils/downcase.rb
@@ -7,7 +7,20 @@ module UnicodeUtils
 
   SPECIAL_DOWNCASE_MAP = Impl.read_special_casing_map("special_lc_map")
 
-  def downcase(str)
+  # Perform a full case-conversion of +str+ to lowercase according to
+  # the Unicode standard.
+  #
+  # Examples:
+  #
+  #     UnicodeUtils.downcase "ᾈ" => "ᾀ"
+  #
+  # Note: The current implementation ignores the +language_id+
+  # argument and doesn't deal with language and context specific
+  # cases. This affects text in the languages Lithuanian, Turkish and
+  # Azeri and the greek letter sigma in a special position. A future
+  # version of UnicodeUtils will fix this. All other languages are
+  # fully supported according to the Unicode standard.
+  def downcase(str, language_id = nil)
     String.new.force_encoding(str.encoding).tap { |res|
       str.each_codepoint { |cp|
         special_mapping = SPECIAL_DOWNCASE_MAP[cp]
--- lib/unicode_utils/upcase.rb
+++ lib/unicode_utils/upcase.rb
@@ -7,7 +7,19 @@ module UnicodeUtils
 
   SPECIAL_UPCASE_MAP = Impl.read_special_casing_map("special_uc_map")
 
-  def upcase(str)
+  # Perform a full case-conversion of +str+ to uppercase according to
+  # the Unicode standard.
+  #
+  # Examples:
+  #
+  #     UnicodeUtils.upcase "weiß" => "WEISS"
+  #
+  # Note: The current implementation ignores the +language_id+
+  # argument and doesn't deal with language and context specific
+  # cases. This affects text in the languages Lithuanian, Turkish and
+  # Azeri. A future version of UnicodeUtils will fix this. All other
+  # languages are fully supported according to the Unicode standard.
+  def upcase(str, language_id = nil)
     String.new.force_encoding(str.encoding).tap { |res|
       str.each_codepoint { |cp|
         special_mapping = SPECIAL_UPCASE_MAP[cp]

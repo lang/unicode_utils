14ac0aedec996587d0b95d9655c49a5006b01ac8
Stefan Lang <langstefan@gmx.at>
2008-11-01 21:47:19 +0100

    US-ASCII suffices for codepoint data

--- lib/unicode_utils/name.rb
+++ lib/unicode_utils/name.rb
@@ -9,6 +9,7 @@ module UnicodeUtils
       Hash.new.tap { |map|
         File.open(path, "r:US-ASCII:-") do |input|
           buffer = "x" * 6
+          buffer.force_encoding(Encoding::US_ASCII)
           while input.read(6, buffer)
             map[buffer.to_i(16)] = input.gets.tap { |x| x.chomp! }
           end
--- lib/unicode_utils/read_codepoint_map.rb
+++ lib/unicode_utils/read_codepoint_map.rb
@@ -7,8 +7,9 @@ module UnicodeUtils
     def self.read_codepoint_map(filename)
       path = File.join(File.dirname(__FILE__), "..", "..", "cdata", filename)
       Hash.new.tap { |map|
-        File.open(path, "r:US-ASCII") do |input|
+        File.open(path, "r:US-ASCII:-") do |input|
           buffer = "x" * 6
+          buffer.force_encoding(Encoding::US_ASCII)
           while input.read(6, buffer)
             map[buffer.to_i(16)] = input.read(6, buffer).to_i(16)
           end

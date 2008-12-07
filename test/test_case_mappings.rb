16abf2bf687bb89381c69e33d5db0b9649cc8608
Stefan Lang <langstefan@gmx.at>
2008-12-07 15:03:29 +0100

    add TestCaseMappings

---
+++ test/test_case_mappings.rb
@@ -0,0 +1,46 @@
+# encoding: utf-8
+
+require "test/unit"
+
+require "unicode_utils/upcase"
+require "unicode_utils/downcase"
+
+class TestCaseMappings < Test::Unit::TestCase
+
+  TXT_DIR = File.dirname(__FILE__)
+
+  def read_txt(filename)
+    File.read(File.join(TXT_DIR, filename), mode: "r:UTF-8:-")
+  end
+
+  def test_upcase_german_text
+    assert_equal read_txt("dreilaendereck_uc.txt"),
+      UnicodeUtils.upcase(read_txt("dreilaendereck.txt"))
+  end
+
+  def test_upcase_german_text_language_de
+    assert_equal read_txt("dreilaendereck_uc.txt"),
+      UnicodeUtils.upcase(read_txt("dreilaendereck.txt"), :de)
+  end
+
+  def test_upcase_german_text_language_tr
+    assert_not_equal read_txt("dreilaendereck_uc.txt"),
+      UnicodeUtils.upcase(read_txt("dreilaendereck.txt"), :tr)
+  end
+
+  def test_downcase_german_text
+    assert_equal read_txt("dreilaendereck_lc.txt"),
+      UnicodeUtils.downcase(read_txt("dreilaendereck.txt"))
+  end
+
+  def test_downcase_german_text_language_de
+    assert_equal read_txt("dreilaendereck_lc.txt"),
+      UnicodeUtils.downcase(read_txt("dreilaendereck.txt"), :de)
+  end
+
+  def test_downcase_german_text_language_tr
+    assert_not_equal read_txt("dreilaendereck_lc.txt"),
+      UnicodeUtils.downcase(read_txt("dreilaendereck.txt"), :tr)
+  end
+
+end

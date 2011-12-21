8e545a97854f93a0691910f37e5e852004d7488c
Stefan Lang <langstefan@gmx.at>
2011-12-21 01:51:35 +0100

    update VERSION doc and bump it to 1.3.0-alpha

--- lib/unicode_utils/version.rb
+++ lib/unicode_utils/version.rb
@@ -4,13 +4,14 @@ module UnicodeUtils
 
   # Corresponds to the unicode_utils gem version.
   #
+  # Conforms to Semantic Versioning as documented at semver.org.
+  #
+  # Summary:
   # MAJOR.MINOR.PATCHLEVEL
   # - A backwards incompatible change causes a change in MAJOR
   # - New features or non-bugfix improvals cause a change in MINOR
   # - Bugfixes increase only PATCHLEVEL.
-  #
-  # A release always has an even PATCHLEVEL. PATCHLEVEL is uneven
-  # during development.
-  VERSION = "1.2.2"
+  # - Pre-release versions append more info after a dash.
+  VERSION = "1.3.0-alpha"
 
 end

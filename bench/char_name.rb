0deb828b734771a74ea3f0062da3796006432ec3
Stefan Lang <langstefan@gmx.at>
2012-03-02 01:52:47 +0100

    bench/char_name.rb: codepoint -> code_point

--- bench/char_name.rb
+++ bench/char_name.rb
@@ -6,38 +6,38 @@ require "unicode_utils/char_name"
 require "unicode_utils/codepoint"
 
 def all_char_names
-  UnicodeUtils::Codepoint::RANGE.each { |codepoint|
-    UnicodeUtils.char_name(codepoint)
+  UnicodeUtils::Codepoint::RANGE.each { |code_point|
+    UnicodeUtils.char_name(code_point)
   }
 end
 
 def cjk_char_names
   [0x3400..0x4DB5, 0x4E00..0x9FC3, 0x20000..0x2A6D6].each { |range|
-    range.each { |codepoint|
-      UnicodeUtils.char_name(codepoint)
+    range.each { |code_point|
+      UnicodeUtils.char_name(code_point)
     }
   }
 end
 
 def hangul_syllable_char_names
-  (0xAC00..0xD7A3).each { |codepoint|
-    UnicodeUtils.char_name(codepoint)
+  (0xAC00..0xD7A3).each { |code_point|
+    UnicodeUtils.char_name(code_point)
   }
 end
 
-def name_map_lookup(codepoint)
-  UnicodeUtils::NAME_MAP[codepoint]
+def name_map_lookup(code_point)
+  UnicodeUtils::NAME_MAP[code_point]
 end
 
 puts "UnicodeUtils.char_name benchmarks"
 
 Benchmark.bm { |x|
   x.report("baseline") {
-    UnicodeUtils::Codepoint::RANGE.each { |codepoint|
-      name_map_lookup(codepoint)
+    UnicodeUtils::Codepoint::RANGE.each { |code_point|
+      name_map_lookup(code_point)
     }
   }
-  x.report("all codepoints") {
+  x.report("all code points") {
     all_char_names
   }
   x.report("CJK UNIFIED IDEOGRAPH") {

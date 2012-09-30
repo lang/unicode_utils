77368724c8509a20556253f89d1e081ecfc37a47
Stefan Lang <langstefan@gmx.at>
2012-09-30 14:03:52 +0200

    test_each_grapheme: - don't create utf-8 strings with surrogates                     - test with utf-16le

--- test/test_each_grapheme.rb
+++ test/test_each_grapheme.rb
@@ -8,7 +8,9 @@ class TestEachGrapheme < Test::Unit::TestCase
 
   UNPAIRED_D800 = [0xd800]
 
-  def each_grapheme_list
+  def each_grapheme_list(encoding = 'utf-8')
+    count = 0
+    skip_count = 0
     fn = File.join(File.dirname(__FILE__),
                    "..", "data", "GraphemeBreakTest.txt")
     File.open(fn, "r:utf-8:-") do |input|
@@ -19,21 +21,26 @@ class TestEachGrapheme < Test::Unit::TestCase
         end
         line.strip!
         next if line.empty?
+        count += 1
         graphemes = line.split("÷").map(&:strip).delete_if(&:empty?)
         graphemes.map! { |g|
           cps = g.split("×").map(&:strip).delete_if(&:empty?).map { |c| c.to_i(16) }
-          has_unpaired_surrogate = true if cps == UNPAIRED_D800
-          cps.inject(String.new.force_encoding('utf-8'), &:<<)
+          if cps == UNPAIRED_D800
+            has_unpaired_surrogate = true
+            skip_count += 1
+            break
+          end
+          cps.inject(String.new.force_encoding(encoding), &:<<)
         }
         # Unpaired surrogates are not allowed in UTF-8
-        # GraphemeBreakTest has test cases with unpaired surrogates which Ruby
-        # 1.9.3 rightfully refuses to iterate with each_codepoint.
+        # GraphemeBreakTest has test cases with unpaired surrogates
         yield graphemes unless has_unpaired_surrogate
       }
     end
+    #print "\nSkipped #{skip_count} out of #{count} grapheme tests due to surrogates\n"
   end
 
-  def test_each_grapheme
+  def test_each_grapheme_utf8
     c = 0
     each_grapheme_list { |grapheme_list|
       c += 1
@@ -41,6 +48,20 @@ class TestEachGrapheme < Test::Unit::TestCase
       UnicodeUtils.each_grapheme(grapheme_list.join) { |g| graphemes << g }
       assert_equal grapheme_list, graphemes
     }
+    assert_equal 288, c
+  end
+
+  def test_each_grapheme_utf16
+    c = 0
+    each_grapheme_list('utf-16le') { |grapheme_list|
+      c += 1
+      graphemes = []
+      UnicodeUtils.each_grapheme(grapheme_list.join) { |g| graphemes << g }
+      assert_equal grapheme_list, graphemes
+    }
+    # TODO: currently we skip the unpaired surrogates for UTF-16 also,
+    # because current Ruby implementations raise an exception in
+    # each_codepoint. Review this point with future implementations.
     assert_equal 288, c
   end
 
